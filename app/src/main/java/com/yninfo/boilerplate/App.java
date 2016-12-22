1e2b3877e23751f2a70c3dda7f24280ffb906187
RainerChao <zzp.doc@hotmail.com>
2016-12-22 15:31:27 +0800

    修改忽略文件

--- app/src/main/java/com/yninfo/boilerplate/App.java
+++ app/src/main/java/com/yninfo/boilerplate/App.java
@@ -2,8 +2,15 @@ package com.yninfo.boilerplate;
 
 import android.app.Application;
 
+import com.orhanobut.logger.Logger;
+
 /**
  * Created by zhaozhiping on 2016/6/17.
  */
 public class App extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Logger.init("").hideThreadInfo().methodCount(3);
+    }
 }
